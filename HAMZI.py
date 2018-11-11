5ee37fcae232e22e98c2e3a6cad5275bc48e7723
Shaikh Tahir <hawkgame4u@gmail.com>
2018-11-11 09:24:11 -0800

    Add files via upload

---
+++ HAMZI.py
@@ -0,0 +1,19 @@
+print(" \t    |  _____     __             __ __               __                    |")
+print(" \t    | / ___/_ __/ /  ___ ____  / // /__ _____    __/ /__ ___              |")
+print("\t    | / /__/ // / _ \/ -_) __/ / _  / _ `(_-< |/|/ /  '_/(_-<             |")
+print("\t    | \___/\_, /_.__/\__/_/   /_//_/\_,_/___/__,__/_/\_\/___/             |")
+print("\t    |   /___/                                               Anonymous     |")
+print("\t    |               Team: | Hawk Hacktivist | Hunk |                      | ")
+
+
+
+user = input("Please Enter Your Name : ")
+x = input("Please Enter Your Age   : ")
+
+if x > 18:
+    print("You Are Eligible For joinnig CYBER HAWKS !")
+    
+else: 
+    print("\nYou Are Not Eligible For Joinning CYBER HAWKS !")
+print("\nYour Name Is : "  + user.title() + " !")
+print("\nYour Age Is  :  " , age )
