fb2230790a015f1e82d6f51c27a0f15e3c0bac7a
Shaikh Tahir <hawkgame4u@gmail.com>
2018-11-15 16:28:24 -0800

    Add files via upload

---
+++ Ipchanger.py
@@ -0,0 +1,95 @@
+
+print(" \t    |  _____     __             __ __               __                    |")
+print(" \t    | / ___/_ __/ /  ___ ____  / // /__ _____    __/ /__ ___              |")
+print("\t    | / /__/ // / _ \/ -_) __/ / _  / _ `(_-< |/|/ /  '_/(_-<             |")
+print("\t    | \___/\_, /_.__/\__/_/   /_//_/\_,_/___/__,__/_/\_\/___/             |")
+print("\t    |   /___/                                               Anonymous     |")
+print("\t    |Made By: | Hawk Hacktivist | Anonymous                               |")
+print("\t    |The Corrupt Fear Us | The Honest Support Us |The Heroic, Join us     |")
+
+
+
+import time 
+import socket
+from requests import get
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+hostname = socket.gethostname() 
+IPAddr = socket.gethostbyname(hostname) 
+ip = get('https://api.ipify.org').text
+s.connect(("8.8.8.8", 80))
+print("\nCurrent Ip ++++++++++++++++++++++++++++ " +s.getsockname()[0]+"++++++++++++++++++++++++++++")
+print("\nIpvc    Ip ++++++++++++++++++++++++++++ " + IPAddr +"++++++++++++++++++++++++++++")
+print("\nPyblic Ip  ===========================> " + ip +      " <===========================")
+s.close()
+
+print("\nPicking Continents:Americas|Antartica|Asia|Austalia|Oceania|NorthAmerica|Europe|Africa")
+print("\nInitializing System Location ! ")
+print("Americas")
+time.sleep(2)
+print("====>Antartica<====")
+time.sleep(2)
+print("====>Asia<====")
+time.sleep(2)
+print("====>Africa<====")
+time.sleep(2)
+print("====>Australia<====")
+time.sleep(2)
+print("====>Oceania<====")
+time.sleep(2)
+print("====>Europe<====")
+time.sleep(2)
+print("====>NorthAmerica<====")
+time.sleep(2)
+print("")
+
+count = 0
+while (count < 900000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000):
+   print('Encrypted New Ip Code ', count)
+   time.sleep(1)
+   count = count + 1
+
+print("Good bye!")
+socks = ("Newly made Sock Proxy For Getting Anonymous")
+
+if socks == 'Americas' or 'AMERICAS' or 'americas' :
+    print("Hello " )
+elif socks == 'Antartica' or 'ANTARTICA' or 'antartica':
+    print("Ip Changed ")
+elif socks == 'Asia' or 'asia' or 'ASIA':
+    print("Asian Ip Changed For Millions")
+elif socks == 'Australia' or 'AUSTRALIA' or 'australia':
+    print("Austalian ip Changed ")
+elif socks == 'Oceania' or 'OCEANIA' or 'oceania':
+    print("Continent Ip Changed ")
+elif socks == 'NorthAmerica' or 'NORTHAMERICA' or 'northamerica':
+    print("Ameicans Ip Changed ")
+elif socks == 'AFRICA' or 'Africa' or 'africa':
+    print("African Ip Changed ")
+else:
+    print("Invalid Entry")
+    
+    
+    def fun(hens):
+        count = 0
+        while (count < 9):
+                  print('The count is:', count)
+                  count = count + 1
+                  print("Good bye!")
+        
+              
+   
+   
+
+
+    
+            
+            
+        return;
+    
+    
+           
+  
+    
+    
